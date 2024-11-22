64f253823e8722e211904923a87f3d3adfbc16a1
GabrielleLingan <chappytony1203@gmail.com>
2024-11-22 11:30:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'students');
+$conn = new mysqli('localhost', 'root', '1234', 'students');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
